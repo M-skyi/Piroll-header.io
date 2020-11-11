dcae08559f7228f361c8f5452952af9445babf8d
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2020-11-11 09:36:59 +0200

    add burger-menu

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 let btnClose = document.querySelector(".btn-close");
 let modalWindow = document.querySelector(".modal");
 let btnSend = document.querySelector(".modal-btn")
-let pageSize = document.querySelector("body").scrollHeight;
-let halfPage = pageSize / 2 ;
+let pageScroll = document.querySelector("body").scrollHeight;
+let halfPage = pageScroll / 2 ;
 
-window.addEventListener("scroll",scr)
-function scr() {
+window.addEventListener("scroll",scrollPage)
+function scrollPage() {
     if (window.pageYOffset >= halfPage) {
         modalWindow.classList.add("show")
     }
 }
 btnClose.addEventListener("click",function() {
     modalWindow.classList.remove("show")
-    window.removeEventListener("scroll",scr)
+    window.removeEventListener("scroll",scrollPage)
 })
 
+let navMobileMenuBtn = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".navigation")
 
+navMobileMenuBtn.addEventListener("click",function () {
+
+    navMobileMenuBtn.classList.toggle("active-menu")
+  if (navMobileMenuBtn.classList.toggle("active-menu")) {
+    mainMenu.classList.remove("active-menu")
+  } if (navMobileMenuBtn.classList.toggle("active-menu")) {
+    mainMenu.classList.add("active-menu")
+  } 
+})
 
 
 
